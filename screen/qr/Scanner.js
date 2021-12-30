7a126c7c9b19a8c5a9c76d74ae7df3e5c6d130a6
ÂRodrigo <rodrigotecuatl@gmail.com>
2021-12-30 16:33:17 -0600

    Se quito y agrego nueva línea de código para probar tutorial de video

--- screen/qr/Scanner.js
+++ screen/qr/Scanner.js
@@ -1,26 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { View, Text, SafeAreaView, StyleSheet, Button, Dimensions, TouchableOpacity } from 'react-native';
+import { Text, SafeAreaView, StyleSheet, Button } from 'react-native';
 import { BarCodeScanner, BarCodeScannerResult } from 'expo-barcode-scanner';
 import BarcodeMask from 'react-native-barcode-mask';
 
 export default function Scanner({ navigation }) {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
-  const [activeCamera, setActiveCamera] = useState(false);
+	// const [ type, setType ] = useState(BarCodeScanner.Constants.back);
 
   useEffect(() => {
     (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
       setHasPermission(status === 'granted');
-      
     })();
-  }, [activeCamera]);
+  }, []);
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
     alert(`Se escaneó el código de barras con el tipo ${type} y los datos ${data}`);
   };
+
+  // const handleBarCodeScanned = ( context ) => {
+  //   setScanned(true);
+  //   alert(`Se escaneó el código de barras con el tipo ${type} y los datos ${data}`);
+  // };
 
   if (hasPermission === null) {
     return <Text>Solicitando permiso de cámara</Text>;
@@ -29,8 +33,6 @@ export default function Scanner({ navigation }) {
     return <Text>Sin acceso a la cámara</Text>;
   }
 
-  console.log(scanned);
-  
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
@@ -38,8 +40,7 @@ export default function Scanner({ navigation }) {
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         type={BarCodeScanner.Constants.Type.back}
         barCodeTypes={[BarCodeScanner.Constants.BarCodeType.qr]}
-        style={StyleSheet.absoluteFillObject} 
-      />
+        style={StyleSheet.absoluteFillObject} />
       <BarcodeMask width={250} height={250} edgeColor="#62B1F6" showAnimatedLine={false} edgeRadius={15} edgeHeight={40} edgeWidth={40} backgroundColor="rgba(0, 0, 0, 0.3)" />
       {scanned && <Button title={'Toque para escanear de nuevo'} onPress={() => setScanned(false)} />}
     </SafeAreaView>
